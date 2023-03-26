de7bbed6ab90389d561620e10c346f54e5ba2fb6
Сергей <pyankov66@mail.ru>
2023-03-26 20:23:48 +0500

    add Example 001-003

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое слагаемое");
+double number1 = Console.ReadLine();
+Console.WriteLine("Введите второе слагаемое");
+double number2 = Console.ReadLine();
+Console.Write(number1 + number2);
+
+int number1 = Random().Next(1,10); // (1,2,3....9)
+Console.WriteLine(number1);
+int number2 = Random().Next(1,10); // (1,2,3....9)
+Console.WriteLine(number2);
+int result = number1 + number2;
+Console.WriteLine(result);
