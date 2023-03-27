199438e23347a4094d9b085292438195d2aea7b1
Сергей <pyankov66@mail.ru>
2023-03-27 11:28:23 +0500

    fix bug Example 003

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,12 +1,13 @@
 ﻿Console.WriteLine("Введите первое слагаемое");
-double number1 = Console.ReadLine();
+string number1 = Console.ReadLine();
+int number5 = number1;
 Console.WriteLine("Введите второе слагаемое");
-double number2 = Console.ReadLine();
+int number2 = Console.ReadLine();
 Console.Write(number1 + number2);
 
-int number1 = Random().Next(1,10); // (1,2,3....9)
+int number3 = new Random().Next(1,10); // (1,2,3....9)
 Console.WriteLine(number1);
-int number2 = Random().Next(1,10); // (1,2,3....9)
+int number4 = new Random().Next(1,10); // (1,2,3....9)
 Console.WriteLine(number2);
 int result = number1 + number2;
 Console.WriteLine(result);
