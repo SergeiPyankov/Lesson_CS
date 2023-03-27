21e2e006b8fe17747f5310d76c3fdc5a3cee0627
Сергей <pyankov66@mail.ru>
2023-03-27 23:22:14 +0500

    add Seminar1_HW

---
+++ Seminar1/WorkHome/Task_002/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое число "); 
+string inputValue1 = Console.ReadLine();
+int number1 = Convert.ToInt32(inputValue1);
+
+Console.Write("Введите второе число "); 
+string inputValue2 = Console.ReadLine();
+int number2 = Convert.ToInt32(inputValue2);
+
+Console.Write("Введите третье число "); 
+string inputValue3 = Console.ReadLine();
+int number3 = Convert.ToInt32(inputValue3);
+
+int max = number1;
+
+if (max < number2)
+{
+    max = number2;
+}
+if (max < number3)
+{
+    max = number3;
+}
+System.Console.WriteLine($"Наибольшее число {max}");
---
+++ Seminar1/WorkHome/Task_003/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите число "); 
+string inputValue = Console.ReadLine();
+int number = Convert.ToInt32(inputValue);
+
+if (number / 2 == int)
+{
+    System.Console.WriteLine("Число является четным");
+}
+System.Console.WriteLine("Число является нечетным");
