4b07b318c1c01be21c9721eb723d5a6019e11ed4
Сергей <pyankov66@mail.ru>
2023-03-27 12:08:08 +0500

    fix bug Example 003

--- Lesson1/Example003_Sum/Program.cs
+++ Lesson1/Example003_Sum/Program.cs
@@ -1,9 +1,11 @@
 ﻿Console.WriteLine("Введите первое слагаемое");
-string number1 = Console.ReadLine();
-int number5 = number1;
+string inputValue1 = Console.ReadLine();
+int number1 = Convert.ToInt32(inputValue1);
 Console.WriteLine("Введите второе слагаемое");
-int number2 = Console.ReadLine();
-Console.Write(number1 + number2);
+string inputValue2 = Console.ReadLine();
+int number2 = Convert.ToInt32(inputValue2);
+Console.Write("Сумма двух чисел равна: ");
+Console.WriteLine (number1 + number2);
 
 int number3 = new Random().Next(1,10); // (1,2,3....9)
 Console.WriteLine(number1);
