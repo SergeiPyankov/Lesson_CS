ede9020ee8e23de3d32745cd44a1a683725a6895
Сергей <pyankov66@mail.ru>
2023-03-30 18:23:07 +0500

    add seminar2 example003

---
+++ Seminar2/Example003/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деление.
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+int Prompt1(string msg)
+{
+    System.Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool Prompt2(int a, int b)
+{
+    if (a % b == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int num1 = Prompt1("Введите 1-е число");
+int num2 = Prompt1("Введите 2-е число");
+
+if (Prompt2(num1, num2))
+{
+    System.Console.WriteLine(num2 + " кратно " + num1);
+}
+else
+{
+    int num3 = num1 % num2;
+    System.Console.WriteLine($"{num2} не кратно {num1} , остаток {num3}");
+}
