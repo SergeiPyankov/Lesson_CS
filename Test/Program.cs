f8d2f3bc177dd6b8726f383325be1a19bd2a1b94
Сергей <pyankov66@mail.ru>
2023-04-02 19:59:33 +0500

    add manual

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,10 +1,1 @@
-﻿void Increment(int n)
-{
-    n++;
-    Console.WriteLine($"Число в методе Increment: {n}");
-}
- 
-int number = 5;
-Console.WriteLine($"Число до метода Increment: {number}");
-Increment(number);
-Console.WriteLine($"Число после метода Increment: {number}");
+﻿
