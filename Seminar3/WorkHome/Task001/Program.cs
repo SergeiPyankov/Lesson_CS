ef6b9ad1eb5c412e26e31f992288dbac0abdf86e
Сергей <pyankov66@mail.ru>
2023-04-14 20:26:51 +0500

    add seminar3 HW Task001 and add manual

--- Seminar3/WorkHome/Task001/Program.cs
+++ Seminar3/WorkHome/Task001/Program.cs
@@ -3,3 +3,41 @@
 // 23432 -> да
 // 12821 -> да
 
+int InputInt(string msg)
+{
+    Console.Write(msg);
+    bool check = int.TryParse(Console.ReadLine(), out int value);
+    if (check)
+    {
+        if (value % 2 != 0)
+        {
+            return value;
+        }
+        Console.WriteLine("Вы ввели четное число");
+        Environment.Exit(1);
+        return 0;
+    }
+    Console.WriteLine("Вы ввели не число");
+    Environment.Exit(2);
+    return 0;
+}
+
+int ReverseNumber(int num)
+{
+    int reverse;
+    for (reverse = 0; num > 0; num = num / 10) reverse = reverse * 10 + num % 10;
+    return reverse;
+}
+
+string CheckPlrm(int val, int rev)
+{
+    if (val == rev)
+    {
+        return "является";
+    }
+    return "не является";
+}
+
+int value = InputInt("Введите нечетное число> ");
+int reverse =  ReverseNumber(value);
+Console.WriteLine($"Вы ввели число: {value}, оно {CheckPlrm(value, reverse)} палиндромом");
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,1 +1,22 @@
 ﻿
+// Console.Write("Введите нечетное число> ");
+// int.TryParse(Console.ReadLine(), out int value);
+// //int value = Convert.Int32(Console.ReadLine());
+// int reverse = 0;
+// string x;
+// for (int i = value; i > 0; i = i / 10) reverse = reverse * 10 + i % 10;
+// if (value == reverse)
+// {
+//     x = "является";
+// }
+// else  x = "не является";
+// Console.WriteLine($"Вы ввели число: {value}, оно {x} палиндромом");
+
+for (int i = 1; i < 10; i++)
+{
+    for (int j = 1; j < 10; j++)
+    {
+        Console.Write($"{i * j} \t");
+    }
+    Console.WriteLine();
+}
