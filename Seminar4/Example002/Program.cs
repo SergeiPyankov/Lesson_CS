dd27d043f154ed387271c3e57b49c5633282cff0
Сергей <pyankov66@mail.ru>
2023-04-15 16:31:17 +0500

    add seminar4 Example002

---
+++ Seminar4/Example002/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int InputInit(string msg)
+{
+    Console.Write(msg);
+    if (int.TryParse(Console.ReadLine(), out int value))
+    {
+        return value;
+    }
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(2);
+    return 0;
+}
+
+int value = InputInit("Введите число> ");
+int i;
+System.Console.Write($"Колличество цифр в числе {value}, ");
+for (i = 0; value > 0; i++) value = value / 10;
+System.Console.WriteLine(i);
---
+++ Seminar4/Example003/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int InputInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int CountNumber(int val)
+{
+    int count;
+    for (int i = 1; i > num; i++) count = i * i++;
+    return count;
+}
+
+int value = InputInt("Введите число> ");
+System.Console.WriteLine($"Произведение чисел от 1 до {value} = {CountNumber(value)}");
