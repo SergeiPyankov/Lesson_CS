36a1ed99fa2c7ebb1ccd14eb278cb5dfbf679c94
Сергей <pyankov66@mail.ru>
2023-04-15 23:25:48 +0500

    add seminar4 Example004 alt

--- Seminar4/Example004/Program.cs
+++ Seminar4/Example004/Program.cs
@@ -1,17 +1,47 @@
 ﻿// Задача 4: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 
-int[] array = new int[8];
-
-void FillArray(int[] arr)
+void Metod1()
 {
-    int lenght = arr.Length;
-    for (int i = 0; i < lenght; i++) arr[i] = new Random().Next(0, 2);
+    int[] FillArray(int size)
+    {
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.Length; i++) arr[i] = new Random().Next(0, 2);
+        return arr;
+    }
+
+    int[] array = FillArray(8);
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Threading.Thread.Sleep(100); // таймаут выполнения
+        System.Console.Write($"{array[i]} ");
+    }
 }
 
-FillArray(array);
-
-for (int i = 0; i < array.Length; i++)
+void Metod2()
 {
-    System.Console.Write($"{array[i]} ");
+    void FillArray(int[] arr)
+    {
+        Random rnd = new Random();
+        //int index = rnd.Next(0, arr.Length + 1) альт ввод
+        for (int i = 0; i < arr.Length / 2; i++)
+        {
+            arr[rnd.Next(0, arr.Length)] = 1;
+            //arr[index] = 1 альт ввод
+        }
+    }
+
+    void PrintArray(int[] arr)
+    {
+        System.Console.Write($"{arr[0]} ");
+        for (int i = 1; i < arr.Length; i++) System.Console.Write($"{arr[i]} ");
+    }
+
+    int[] array = new int[8];
+    FillArray(array);
+    PrintArray(array);
 }
+
+
+Metod2();
