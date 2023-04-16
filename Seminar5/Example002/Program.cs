348ff881abc5688ab08fa2634717a6da17dce9c2
Сергей <pyankov66@mail.ru>
2023-04-16 20:19:14 +0500

    add seminar5 Example002

--- Seminar5/Example002/Program.cs
+++ Seminar5/Example002/Program.cs
@@ -1,2 +1,59 @@
 ﻿// Задача 2: Напишите программу замены элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
 // [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+void Metod1()
+{
+    int[] PrintRandomArray(int size)
+    {
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.Length; i++) arr[i] = new Random().Next(-9, 10);
+        Console.WriteLine($"Случайно сгенерированный массив из {size} элеменетов => ");
+        for (int i = 0; i < arr.Length; i++) Console.Write(arr[i] + ", ");
+        return arr;
+    }
+
+    int InputInt(string msg)
+    {
+        Console.Write(msg);
+        if (int.TryParse(Console.ReadLine(), out int num))
+        {
+            return num;
+        }
+        Console.WriteLine("Вы ввели не число");
+        Environment.Exit(1);
+        return 0;
+    }
+
+
+    int[] PrintReverseNumArr(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++) arr[i] = arr[i] - arr[i] - arr[i];
+        Console.WriteLine($"Инверсия массива => ");
+        for (int i = 0; i < arr.Length; i++) Console.Write(arr[i] + ", ");
+        return arr;
+    }
+
+    int size = InputInt("Введите длину массива ");
+    int[] array = PrintRandomArray(size);
+    System.Console.WriteLine();
+    PrintReverseNumArr(array);
+}
+
+void Metod2()
+{
+    int[] PrintRandomArray(int size)
+    {
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.Length; i++) arr[i] = new Random().Next(-9, 10);
+        Console.WriteLine($"Случайно сгенерированный массив из {size} элеменетов => ");
+        for (int i = 0; i < arr.Length; i++) Console.Write(arr[i] + ", ");
+        System.Console.WriteLine();
+        Console.WriteLine($"Инверсия массива => ");
+        for (int i = 0; i < arr.Length; i++) Console.Write(arr[i] - arr[i] - arr[i] + ", ");
+        return arr;
+    }
+
+    PrintRandomArray(10);
+}
+
+Metod2();
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,22 +1,3 @@
-﻿
-// Console.Write("Введите нечетное число> ");
-// int.TryParse(Console.ReadLine(), out int value);
-// //int value = Convert.Int32(Console.ReadLine());
-// int reverse = 0;
-// string x;
-// for (int i = value; i > 0; i = i / 10) reverse = reverse * 10 + i % 10;
-// if (value == reverse)
-// {
-//     x = "является";
-// }
-// else  x = "не является";
-// Console.WriteLine($"Вы ввели число: {value}, оно {x} палиндромом");
-
-for (int i = 1; i < 10; i++)
-{
-    for (int j = 1; j < 10; j++)
-    {
-        Console.Write($"{i * j} \t");
-    }
-    Console.WriteLine();
-}
+﻿int x = 9;
+x = x - x - x;
+System.Console.WriteLine(x);
