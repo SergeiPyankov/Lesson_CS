9810d5760469beec4369b8784a366c828cbc750e
Сергей <pyankov66@mail.ru>
2023-04-16 21:47:04 +0500

    add seminar5 Example004

--- Seminar5/Example003/Program.cs
+++ Seminar5/Example003/Program.cs
@@ -1,3 +1,39 @@
 ﻿// Задача 3: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
 // 4; массив [6, 7, 19, 345, 3] -> нет
 // 3; массив [6, 7, 19, 345, 3] -> да
+
+int InputInt(string msg)
+{
+    Console.Write(msg);
+    if (int.TryParse(Console.ReadLine(), out int num))
+    {
+        return num;
+    }
+    Console.WriteLine("Вы ввели не число");
+    Environment.Exit(1);
+    return 0;
+}
+
+int[] PrintRandomArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++) arr[i] = new Random().Next(1, 10);
+    Console.Write($"Случайно сгенерированный массив из {size} элементов от 1 до 9: ");
+    for (int i = 0; i < arr.Length; i++) Console.Write(arr[i] + ", ");
+    return arr;
+}
+
+string SearchUserNum(int[] arr, int number)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == number)
+        return "Yes";
+    }
+    return "No";
+}
+
+int sizeArray = InputInt("Введите длину массива => ");
+int userNumber = InputInt("Введите искомое число => ");
+int[] array = PrintRandomArray(sizeArray);
+Console.WriteLine("=> " + SearchUserNum(array, userNumber));
--- Seminar5/Example004/Program.cs
+++ Seminar5/Example004/Program.cs
@@ -1,5 +1,25 @@
-﻿// Задача 4: Задайте одномерный массив из 10 случайных чисел в диапазоне от 1 до 200. Найдите количество  двузначных элементов массива. 
+﻿// Задача 4: Задайте одномерный массив из 10 случайных чисел в диапазоне от 1 до 200. Найдите количество двузначных элементов массива. 
 // Пример для массива из 5, а не 10 элементов. В своём решении сделайте для 10
 // [5, 18, 123, 6, 2] -> 1
 // [1, 2, 3, 6, 2] -> 0
 // [10, 11, 12, 13, 14] -> 5
+
+int[] PrintRandomArray()
+{
+    int[] arr = new int[10];
+    for (int i = 0; i < arr.Length; i++) arr[i] = new Random().Next(1, 201);
+    Console.Write($"Случайно сгенерированный массив из 10 элементов от 1 до 200: ");
+    for (int i = 0; i < arr.Length; i++) Console.Write(arr[i] + ", ");
+    System.Console.WriteLine();
+    return arr;
+}
+
+int SearchNumber(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++) if (arr[i] > 9 && arr[i] < 100) count++;
+    return count;
+}
+
+int[] array = PrintRandomArray();
+System.Console.Write($"Количество двузначных чисел в массиве: {SearchNumber(array)}");
